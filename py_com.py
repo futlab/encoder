5fb2c53915824b3ccbc705a9f5d135e89af653e9
Danil <vk.vc@mail.ru>
2018-07-19 12:20:34 +0500

    fix for python3 pyserial module

--- py_com.py
+++ py_com.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import rospy
 from std_msgs.msg import Float32
 import serial as srl
@@ -18,9 +18,10 @@ for i in range(0,last_number_of_port_name+1):
         ser.baudrate = 115200
         print("Well done!")
         ser.write(cm_id)
-        time.sleep(0.1)
+        time.sleep(0.5)
         while ser.inWaiting()>0 and len(res)<=2:
-            res += ser.read(1)
+            res += (ser.read(1)).decode()
+            print(res)
         if res=="03":
             print("Device with id=03 found!")
             break
@@ -38,24 +39,20 @@ for i in range(0,last_number_of_port_name+1):
 
 pub = rospy.Publisher('/wheel_module', Float32, queue_size=10)
 rospy.init_node('wheel_module')
-#rate = rospy.Rate(10) # 10hz
 
-if ser.isopen():
+res = ""
+cm_teak = "/teak;".encode()
+time_start = time.time()
+print("Well done!")
+while True:
+    ser.write(cm_teak)
+    time.sleep(0.025)
+    while ser.inWaiting() > 0:
+        res += (ser.read(1)).decode()
+    less, numb = res.split(" ")
+    if not rospy.is_shutdown():
+        rospy.loginfo(float(numb))
+        pub.publish(float(numb))
+        #rate.sleep()
+    print("{"+res+" "+str(time.time()-time_start)+" seconds }")
     res = ""
-    cm_teak = "/teak;".encode()
-    time_start = time.time()
-    print("Well done!")
-    while True:
-        ser.write(cm_teak)
-        time.sleep(0.025)
-        while ser.inWaiting() > 0:
-            res += ser.read(1)
-        less, numb = res.split(" ")
-        if not rospy.is_shutdown():
-            rospy.loginfo(float(numb))
-            pub.publish(float(numb))
-            #rate.sleep()
-        print("{"+res+" "+str(time.time()-time_start)+" seconds }")
-        res = ""
-else:
-    print("Serial is not open")
